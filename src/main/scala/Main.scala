eefff0399dde7b93519d2d71f60463a1bad1dfd0
aharrah95 <abigail.harrah@revature.net>
2022-02-15 16:15:44 -0600

    Hufflepuff wins this time!

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,11 @@
 import org.apache.spark._
 import org.apache.spark.streaming._
 import org.apache.spark.sql._
+import abby.Whatever
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Start of P2")
+    println("Slytherin Wins! (Damn it...)")
 
   //  val sconf = new SparkConf().setMaster("local[4]").setAppName("P2").setSparkHome("C:\\Spark")
 //    val sc = new SparkContext(sconf)
@@ -14,6 +15,6 @@ object Main {
 
     var df = ssql.sql("SELECT * FROM Wildfire")
     df.show()
-
+    Whatever.testprint()
   }
 }
