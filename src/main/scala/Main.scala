dd40d44ff9254bf451fd2a28c0d9b3cc9d822f89
yueqi <yueqi.peng@revature.net>
2022-02-17 13:37:54 -0600

    import weather data

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,7 @@ import org.apache.spark._
 import org.apache.spark.streaming._
 import org.apache.spark.sql._
 import yueqi.GetWeather
+import yueqi.Chisquare
 import abby.Whatever
 import org.apache.spark.sql.DataFrameWriter
 // import org.apache.spark.sql.SparkSession.implicits._
@@ -10,14 +11,14 @@ import dataops.DataOps
 
 object Main {
   def main(args: Array[String]): Unit = {
-    System.setProperty("hadoop.home.dir", "C:\\hadoop")
+    //System.setProperty("hadoop.home.dir", "C:\\hadoop")
     // val sconf = new SparkConf().setMaster("local[4]").setAppName("Wildfire").setSparkHome("C:\\Spark")
     // sconf.set("spark.driver.memory", "4g") 
     
     // val sc = new SparkContext(sconf)
     
     // val ssql = SparkSession.builder().appName("Wildfire").config("spark.master", "local").config("spark.driver.memory", "4g").config("spark.executor.memory", "4g").enableHiveSupport().getOrCreate()
-    val ssql = SparkSession.builder().appName("Wildfire").config("spark.master", "local").config("spark.driver.memory", "4g").enableHiveSupport().getOrCreate()
+    //val ssql = SparkSession.builder().appName("Wildfire").config("spark.master", "local").config("spark.driver.memory", "4g").enableHiveSupport().getOrCreate()
     
     //Whatever.testprint()
     //Yueqi.testprint()
@@ -25,7 +26,8 @@ object Main {
     //val sconf = new SparkConf().setMaster("local[4]").setAppName("P2").setSparkHome("C:\\Spark")
     //val sc = new SparkContext(sconf)
 
-    GetWeather.getWeather()
+    GetWeather.getWeather("dataset/fireG.parquet", "dataset/testweather.csv")
+    //Chisquare.fireSizeAndCause()
 
     // Test comment for a test commit for brandon to test pull request and merge master into brandon
     // Test comment two for test commit two
